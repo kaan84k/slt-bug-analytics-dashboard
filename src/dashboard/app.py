2de990150df0eb7c8afe994897345c8a24cc29ec
Kavindu Ranganth <91959867+Rajapaksha99@users.noreply.github.com>
2025-10-02 12:08:39 +0530

    Update app.py

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -62,7 +62,7 @@ SYSLOG_COLORS = {
 }
 
 # Set page configuration first to avoid StreamlitAPIException
-st.title("SLT Selfcare App - Bug ")
+st.title("SLT Selfcare App - Bug Analytics Dashboard")
 
 # --- Load and process data ---
 try:
